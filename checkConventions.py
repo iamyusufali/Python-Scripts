0cada7175b7d5598bb1582d1c3e31c3035778858
Yusuf Ali <yali98319@gmail.com>
2020-03-23 19:07:54 +0530

    Updated Script

---
+++ checkConventions.py
@@ -0,0 +1,66 @@
+# Importing glob and RegEx
+import glob as FileManager
+import re
+
+
+# Prompt for Directory name and location
+Directory = input("Enter directory name: ")
+Location = input("Enter directory location: ")
+
+
+# Patterns and Replacements
+pattern = [
+  r'([\w]+)(\(([\w, \w]+)?\){)',
+  r'([\w]+)( \(([\w, \w]+)?\){)',
+  r'([\w]+)(\(([\w, \w]+)?\) {)',
+  r'({)([\w:"\', ]+)(})',
+  r'(\w)(:)([\'"])',
+  r'([\w"\']+)(,)([\'"\w]+)',
+  r'(={)',
+  r'(=\[)'
+]
+
+replacement = [
+  r'\1 (\3) {',
+  r'\1 (\3) {',
+  r'\1 (\3) {',
+  r'{ \2 }',
+  r'\1: \3',
+  r'\1, \3',
+  r'= {',
+  r'= ['
+]
+
+
+# Open file and replace each matching pattern with the replacement 
+def addCorrectIndents(file):
+  num = 0
+  while num < len(pattern):
+    # Reads the file and replaces the matching pattern 
+    with open(file, 'r') as rf:
+      content = rf.read()
+      RegEx = re.compile(pattern[num])
+      data = RegEx.sub(replacement[num], content)
+      # Writes the new data to the file
+      with open(file, 'w') as wf:
+        wf.write(data)
+      num += 1
+      
+
+# Recursively find every JS file and store it
+Files = FileManager.glob(Location + Directory + '/**/*.js', recursive = True)
+
+
+# Add indents to every file
+for file in Files:
+  addCorrectIndents(file)
+
+
+
+# import re
+# string = '{title:"Yusuf",name:"Ali"}'
+# RegEx = re.compile(r'({)([\w:"\',]+)(})')
+# result = RegEx.search(string)
+# change = RegEx.sub(r'{ \2 }', string)
+# print(result)
+# print(change)
